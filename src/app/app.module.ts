3080f73c733d23e90302e8fcfc46b761407b46ad
Ibrahim Merizak <72378125+imerizak@users.noreply.github.com>
2021-02-05 23:39:35 +0100

    Perso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
 import { PersoPageComponent } from './perso-page/perso-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +22,11 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
