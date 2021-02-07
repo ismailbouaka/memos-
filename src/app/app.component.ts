79d642996e6fb790f6925b92b88618b4197d8f08
Ibrahim Merizak <72378125+imerizak@users.noreply.github.com>
2021-02-07 16:17:36 +0100

    hj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { SharedService } from './service/Shared.service';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +11,12 @@ import * as firebase from 'firebase';
 export class AppComponent implements OnInit{
   title = 'memos';
   isconnected: Boolean = false;
+  ss: SharedService
+  constructor(ss: SharedService){
+    this.ss = ss
+  }
 
   ngOnInit(){
-    const user = firebase.default.auth().currentUser;
-
-    if(user){
-      this.isconnected = true;
-
-    }else{
-      this.isconnected = false;
-    }
+    this.ss.getEmittedValue().subscribe((item: Boolean) => this.isconnected = item)
   }
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { SharedService } from './../service/Shared.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service'
 import { User } from '../service/user'
@@ -13,20 +14,23 @@ import { Router } from '@angular/router'
 export class HomePageComponent implements OnInit {
 
   user: User;
+  ss: SharedService
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, ss: SharedService) {
     this.user = new User();
+    this.ss = ss;
   }
 
   ngOnInit(): void {
   }
 
 
+
   login() {
     console.log(this.user.email + this.user.password)
     this.authService.login(this.user.email, this.user.password).then(val => {
       //this.router.navigate(['']);
-      window.location.reload();
+      this.ss.change();
       console.log("you logged in")
     }).catch(error => console.log("erreur :" + error));
   }
